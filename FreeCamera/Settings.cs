f508642685a6b62f674b1bbbb0c99ff98ecb82f3
aedenthorn <aedenthorn1@gmail.com>
2021-09-20 20:37:12 -0400

    etc

--- FreeCamera/Settings.cs
+++ FreeCamera/Settings.cs
@@ -4,8 +4,8 @@ namespace FreeCamera
 {
     public class Settings : UnityModManager.ModSettings
     {
-        public string ZRotateModKey { get; set; } = "left shift";
-        public string XRotateModKey { get; set; } = "left ctrl";
+        public string XRotateModKey { get; set; } = "left shift";
+        public string ZRotateModKey { get; set; } = "left ctrl";
         public string RotateLeftKey { get; set; } = "q";
         public string RotateRightKey { get; set; } = "e";
         public override void Save(UnityModManager.ModEntry modEntry)
